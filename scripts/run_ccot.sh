4c23169e9dfddf88ebd23a2215a6e2676fb38c66
421zuoduan <ruochencui421@gmail.com>
2025-01-15 22:43:24 +0800

    bash update

--- scripts/run_ccot.sh
+++ scripts/run_ccot.sh
@@ -1,4 +1,4 @@
 model="gpt3.5-turbo"
 dataset="gsm8k"
 # dataset="multiarith"
-python ccot_basedon_auto.py --method=contrast_cot --model=${model} --dataset=${dataset}
+python ccot.py --method=contrast_cot --model=${model} --dataset=${dataset}
